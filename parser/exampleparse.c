011a60c84ae9b2acd71210cb0460d2a56bebca32
Rebecca Friedman <rsf92@gwmail.gwu.edu>
2013-12-05 01:26:07 -0500

    parser!

---
+++ parser/exampleparse.c
@@ -0,0 +1,96 @@
+#include   <stdio.h>
+#include   <string.h>
+#include   <stdlib.h>
+#include   <libxml/xmlmemory.h>
+#include   <libxml/parser.h>
+
+void
+parseStory (xmlDocPtr doc, xmlNodePtr cur, xmlChar **path) {
+/* first output should be /storyinfo/author/John */		
+	xmlNodePtr level;
+	xmlChar *path2[1000];
+	xmlChar *myfile;
+
+	FILE *file = NULL;
+	file = fopen("/mnt/hgfs/Project/fuse-2.6.3/project/path.txt", "a");
+
+	strcat(path, cur->name);
+	strcat(path, "/");
+	
+	fprintf(file, "%s\n", path);
+	//fclose(file);
+	//printf("%s\n", path);
+
+	/* check and see if there is a child. if not get file, if so continue normally*/
+
+	if((cur->xmlChildrenNode->next)==NULL){	
+		myfile = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+		strcat(path, myfile);
+		fprintf(file, "%s\n", path);
+		//fclose(file);
+		//printf("%s\n", path);
+	}
+	else{
+		level = cur->xmlChildrenNode->next;
+		while(level!=NULL){
+			strcpy(path2, path);	
+			parseStory(doc, level, path2);
+			level = level->next->next;	
+		}
+	}
+//printf("cur: %s\n", cur->name);
+//printf("next cur: %s\n", cur->next->name);
+
+}
+static void
+parseDoc(char *docname) {
+        xmlDocPtr doc;
+        xmlNodePtr cur;
+	xmlChar *path[1000];
+	xmlChar* mylevel;
+
+        doc = xmlParseFile(docname);
+        if (doc == NULL ) {
+                fprintf(stderr,"Document not parsed successfully. \n");
+                return;
+        }
+        cur = xmlDocGetRootElement(doc);
+        if (cur == NULL) {
+                fprintf(stderr,"empty document\n");
+                xmlFreeDoc(doc);
+                return;
+        }
+        if (xmlStrcmp(cur->name, (const xmlChar *) "root")) {
+                fprintf(stderr,"document of the wrong type, root node != root");
+                xmlFreeDoc(doc);
+                return;
+        }
+        cur = cur->xmlChildrenNode;
+	mylevel = cur->next->name;
+	//printf("mylevel: %s\n", mylevel);
+        while (cur != NULL) {
+                if ((!xmlStrcmp(cur->name, (const xmlChar *)mylevel))){
+			//printf("mycurname: %s\n", cur->name);   
+			strcpy(path, "/");                     
+			 parseStory (doc, cur, path);
+			if((cur->next->next)!=NULL){
+			strcpy(path, "/");
+			mylevel=cur->next->next->name;
+                	}	
+		}
+        cur = cur->next;
+        }
+        xmlFreeDoc(doc);
+        return;
+}
+int
+main(int argc, char **argv) {
+        char *docname;
+        if (argc <= 1) {
+                printf("Usage: %s docname\n", argv[0]);
+                return(0);
+        }
+        docname = argv[1];
+        parseDoc (docname);
+        return (1);
+}
