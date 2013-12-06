77261fcff5ef3841cd9cb18cc4234463cf396fe6
rolivia <rolivia@gwmail.gwu.edu>
2013-12-06 15:58:53 -0500

    FS

--- FS.c
+++ FS.c
@@ -5,401 +5,399 @@
     This program can be distributed under the terms of the GNU GPL.
     See the file COPYING.
 
-    gcc -Wall `pkg-config fuse --cflags --libs` fusexmp.c -o fusexmp
+    gcc -Wall `pkg-config fuse --cflags --libs` hello.c -o hello
 */
 
 #define FUSE_USE_VERSION 26
 
-#include <config.h>
-
-#ifdef linux
-/* For pread()/pwrite() */
-#define _XOPEN_SOURCE 500
-#endif
-
 #include <fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include <errno.h>
-#include <sys/time.h>
-#ifdef HAVE_SETXATTR
-#include <sys/xattr.h>
-#endif
+#include <fcntl.h>
 
-static int xmp_getattr(const char *path, struct stat *stbuf)
-{
+static const char *hello_str = "Hello World!\n";
+static const char *hello_path = "/hello";
 
-    printf("cd here\n");
-    int res;
+typedef struct node{
+	char* name;
+	struct node *nextSib;
+	struct node *firstChild;
+	char type; /* 'd' == directory; 'f' == file */
+}NODE;
 
-    res = lstat(path, stbuf);
-    if (res == -1)
-        return -errno;
+typedef struct tree{
+	NODE* root;
+	int depth;
+}TREE;
 
-    return 0;
-}
- 
-static int xmp_access(const char *path, int mask)
-{
-    int res;
+/* given a string, return the index of the first instance of the '/' char */
+int index_of_first_slash(char* str){
+	char tmp = *str;
+	int i = 0;
 
-    res = access(path, mask);
-    if (res == -1)
-        return -errno;
+	while(tmp != '\0'){
+		if(tmp == '/') return i;
+		i++;
+		tmp = *(str+i);
 
-    return 0;
-}
-
-static int xmp_readlink(const char *path, char *buf, size_t size)
-{
-    int res;
-
-    res = readlink(path, buf, size - 1);
-    if (res == -1)
-        return -errno;
-
-    buf[res] = '\0';
-    return 0;
+	}
+	return -1; /* '/' not found in str */
 }
 
+/* given a tree and a path, return the node pointer associated with that path 
+ * return null if the path does not exist
+ */
+NODE* get_node(TREE* tree, char* path){
+	char *dir, *tmp;
+	int pathLength, idOfSlash1;
+	NODE* curNode;
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                       off_t offset, struct fuse_file_info *fi)
+	if(tree == NULL){
+		printf("tree is empty\n");
+		return NULL;
+	}else if(tree->root == NULL){
+		printf("tree is not initialized\n");
+		return NULL;
+	}
+
+	pathLength = strlen(path);
+
+	dir = (char*)malloc(sizeof(char) * pathLength);
+	tmp = (char*)malloc(sizeof(char) * pathLength);
+	strcpy(tmp, path);
+
+	curNode = tree->root;
+
+	if(tmp[0] == '/'){ //take care of leading slash
+		strcpy(dir, "/");
+		strcpy(tmp, tmp+1);
+	}
+
+	while(strcmp(tmp, "") != 0){
+		if(curNode == NULL){
+			printf("ERROR: path DNE");
+			return NULL;
+		}
+		while(strcmp(curNode->name, dir) != 0){
+			curNode = curNode->nextSib;
+			if(curNode == NULL){
+				printf("ERROR: path DNE");
+				return NULL;
+			}
+		}
+		idOfSlash1 = index_of_first_slash(tmp);
+		strncpy(dir, tmp, idOfSlash1);
+		strncpy(tmp, tmp + idOfSlash1 + 1, pathLength - idOfSlash1);
+		curNode = curNode->firstChild;
+	}
+	if(strcmp(curNode->name, dir) == 0){
+		return curNode;
+	}
+	else{
+		printf("ERROR: path DNE");
+		return NULL;
+	}
+}
+
+TREE* create_tree(){
+	
+	TREE* res = (TREE*)malloc(sizeof(TREE));
+	NODE* root = (NODE*)malloc(sizeof(NODE));
+
+	if(res == NULL)        printf("error allocating tree\n");
+	else if(root == NULL)  printf("error allocating root\n");
+	else{
+		/* root of every tree is "/" */
+		root->name = "/";
+		root->nextSib = NULL;
+		root->firstChild = NULL;
+		root->type = 'd';
+		
+		res->root = root;
+		res->depth = 1;
+	}
+
+	return res;
+}
+
+/* given a path and a tree, create a new node, 
+ * and add it to the tree in correct place (rep'd by location)
+ */
+ /*******TODO: ERROR CHECK - IF PATH TO LOCATION DNE -> PRINT ERROR AND RETURN*******/
+void add_node(TREE* tree, char* location){
+	int slashIndex, location_length;
+	char *dir, *tmp;
+	NODE *parent, *child;
+	int pathLength = 0;
+
+	/* allocate memory for new_node */
+	NODE* new_node = (NODE*)malloc(sizeof(NODE));
+	if(new_node == NULL){
+		printf("error allocating node\n");
+		return;
+	}
+
+	location_length = strlen(location);
+	dir = (char*)malloc(sizeof(char)*location_length);
+	tmp = (char*)malloc(sizeof(char)*location_length);
+
+	if(location[location_length-1] == '/'){
+		new_node->type = 'd';
+		/*remove last '/' */
+		strncpy(tmp, location, location_length-1);
+	}else{
+		new_node->type = 'f';
+		strcpy(tmp, location);
+	}
+
+	/* initialize new_node */
+	new_node->nextSib = NULL;
+	new_node->firstChild = NULL;
+
+	if(tree->root == NULL){  /*tree is empty*/
+		tree->root = new_node;
+		tree->depth = 1;
+	}
+	else{
+		slashIndex = index_of_first_slash(location);
+		if(slashIndex != -1){
+			parent = tree->root;
+			while(slashIndex != -1 && slashIndex != (strlen(tmp) - 1)){
+			/*divide string in 2 pieces --> dir=before '/'; tmp=after '/'; discard '/'*/
+				strncpy(dir, tmp, slashIndex);
+				strncpy(tmp, tmp + slashIndex + 1, location_length - slashIndex);
+
+				if(strcmp(dir, "") == 0){
+				/* make root the parent */
+					parent = tree->root;
+					pathLength = 1;
+				}else if(strcmp(dir, parent->name) != 0){
+				/* loop through children of tmpNode to find dir */
+					if(parent->firstChild != NULL){
+						parent = parent->firstChild;
+						pathLength++;
+					}
+					while(strcmp(parent->name, dir) != 0){   
+						parent = parent->nextSib;
+						if(parent == NULL){
+							printf("Error parent %s could not be found in tree. Error on path: %s\n", dir, location);
+							return;
+						}
+					}
+				}
+				slashIndex = index_of_first_slash(tmp);
+			}
+			/*DIR == PARENT OF TMP       TMP == NAME OF NEW NODE*/
+			/* add new_node to the list of children of parent */
+			new_node->name = tmp;
+			if(parent->firstChild == NULL){ /*special case: parent has no children */
+				parent->firstChild = new_node;
+				pathLength++;
+			}else{
+				child = parent->firstChild;
+				while(child->nextSib != NULL){ /*find last child node in the linked list*/
+					if(strcmp(tmp, child->name) == 0){
+						printf("Error: this element already exists\tNode name = %s\n", child->name);
+						return;
+					}
+					child = child->nextSib;
+				}
+				child->nextSib = new_node;
+				pathLength++;
+			}
+		}
+	}
+	if(pathLength > tree->depth) tree->depth = pathLength;
+
+}
+
+void printAllChildren(TREE* tree, char* path){
+	NODE* dir;
+	NODE* child;
+
+	dir = get_node(tree, path);
+	if(dir == NULL){
+		printf("ERROR: Path does not exist\n");
+		return;
+	}
+
+	child = dir->firstChild;
+	if(child == NULL){
+		printf("%s is empty!\n", dir->name);
+	}else{
+		while(child != NULL){
+			printf("%s\n", child->name);
+			child = child->nextSib;
+		}
+	}
+}
+
+/* helper function to print tree recursively */
+/*TODO: consider changing to iterative function*/
+void print_tree_recursive(TREE* tree, NODE* root, char* path){
+	NODE* tmp;
+	char* path2;
+
+	path2 = (char*)malloc(sizeof(char)*500);
+
+	//strcat(path, "/");
+	strcat(path, root->name);
+	if(root->type == 'd' && strcmp(root->name, "/") != 0)		strcat(path, "/");
+	
+	printf("Path of %s = %s\n", root->name, path);
+
+	tmp = root->firstChild;
+	while(tmp != NULL){
+		strcpy(path2,path);
+		print_tree_recursive(tree,tmp,path2);
+		tmp = tmp->nextSib;
+	}
+
+	free(path2);
+}
+
+/* given a tree: print the path for each node in the tree */
+void print_tree(TREE* tree){
+	char* path;
+
+	path = (char*)malloc(sizeof(char) * 500);
+
+	print_tree_recursive(tree, tree->root, path);
+	free(path);
+}
+
+void destroy_tree_recurssive(TREE* tree, NODE* node){
+	//base case: leaf & last sib
+	if(node->firstChild == NULL && node->nextSib == NULL){
+		free(node);
+		return;
+	}else if(node->nextSib == NULL){ //recurssive case 1: last Sib ONLY -> remove node and all children
+		destroy_tree_recurssive(tree, node->firstChild);
+		free(node);
+		return;
+	}else if(node->firstChild == NULL){ //recurssive case 3: leaf ONLY -> remove node and all sibs
+		destroy_tree_recurssive(tree, node->nextSib);
+		free(node);
+		return;
+	}else{ //recurssive case 3: node has sibs and cildren -> remove node & all sibs & all children
+		destroy_tree_recurssive(tree, node->nextSib);
+		destroy_tree_recurssive(tree, node->firstChild);
+		free(node);
+		return;
+	}
+}
+
+void destroy_tree(TREE* tree){
+	destroy_tree_recurssive(tree,tree->root);
+}
+
+
+static int hello_getattr(const char *path, struct stat *stbuf)
 {
-printf("ls here\n");
-    DIR *dp;
-    struct dirent *de;
+  printf("hello_getattr\n");
+    int res = 0;
 
+    memset(stbuf, 0, sizeof(struct stat));
+    if(strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    }
+    else if(strcmp(path, hello_path) == 0) {
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = strlen(hello_str);
+    }
+    else
+        res = -ENOENT;
+
+    return res;
+}
+
+static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                         off_t offset, struct fuse_file_info *fi)
+{
+  printf("hello_readdir\n");
     (void) offset;
     (void) fi;
 
-    dp = opendir(path);
-    if (dp == NULL)
-        return -errno;
+    if(strcmp(path, "/") != 0)
+        return -ENOENT;
 
-    while ((de = readdir(dp)) != NULL) {
-        struct stat st;
-        memset(&st, 0, sizeof(st));
-        st.st_ino = de->d_ino;
-        st.st_mode = de->d_type << 12;
-        if (filler(buf, de->d_name, &st, 0))
-            break;
-    }
-
-    closedir(dp);
-    return 0;
-}
-
-static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
-{
-    int res;
-
-    /* On Linux this could just be 'mknod(path, mode, rdev)' but this
-       is more portable */
-    if (S_ISREG(mode)) {
-        res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
-        if (res >= 0)
-            res = close(res);
-    } else if (S_ISFIFO(mode))
-        res = mkfifo(path, mode);
-    else
-        res = mknod(path, mode, rdev);
-    if (res == -1)
-        return -errno;
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
+    filler(buf, hello_path + 1, NULL, 0);
 
     return 0;
 }
 
-static int xmp_mkdir(const char *path, mode_t mode)
+static int hello_open(const char *path, struct fuse_file_info *fi)
 {
-printf("mkdir here\n");
-    int res;
+  printf("hello_open\n");
+    if(strcmp(path, hello_path) != 0)
+        return -ENOENT;
 
-    res = mkdir(path, mode);
-    if (res == -1)
-        return -errno;
+    if((fi->flags & 3) != O_RDONLY)
+        return -EACCES;
 
     return 0;
 }
 
-static int xmp_unlink(const char *path)
+static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi)
 {
-    int res;
-
-    res = unlink(path);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_rmdir(const char *path)
-{
-printf("rmdir here\n");
-    int res;
-
-    res = rmdir(path);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_symlink(const char *from, const char *to)
-{
-    int res;
-
-    res = symlink(from, to);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_rename(const char *from, const char *to)
-{
-    int res;
-
-    res = rename(from, to);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_link(const char *from, const char *to)
-{
-    int res;
-
-    res = link(from, to);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_chmod(const char *path, mode_t mode)
-{
-    int res;
-
-    res = chmod(path, mode);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_chown(const char *path, uid_t uid, gid_t gid)
-{
-    int res;
-
-    res = lchown(path, uid, gid);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_truncate(const char *path, off_t size)
-{
-    int res;
-
-    res = truncate(path, size);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_utimens(const char *path, const struct timespec ts[2])
-{
-    int res;
-    struct timeval tv[2];
-
-    tv[0].tv_sec = ts[0].tv_sec;
-    tv[0].tv_usec = ts[0].tv_nsec / 1000;
-    tv[1].tv_sec = ts[1].tv_sec;
-    tv[1].tv_usec = ts[1].tv_nsec / 1000;
-
-    res = utimes(path, tv);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_open(const char *path, struct fuse_file_info *fi)
-{
-printf("open/edit file here\n");
-    int res;
-
-    res = open(path, fi->flags);
-    if (res == -1)
-        return -errno;
-
-    close(res);
-    return 0;
-}
-
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-                    struct fuse_file_info *fi)
-{
-    int fd;
-    int res;
-
+  printf("hello_read\n");
+    size_t len;
     (void) fi;
-    fd = open(path, O_RDONLY);
-    if (fd == -1)
-        return -errno;
+    if(strcmp(path, hello_path) != 0)
+        return -ENOENT;
 
-    res = pread(fd, buf, size, offset);
-    if (res == -1)
-        res = -errno;
+    len = strlen(hello_str);
+    if (offset < len) {
+        if (offset + size > len)
+            size = len - offset;
+        memcpy(buf, hello_str + offset, size);
+    } else
+        size = 0;
 
-    close(fd);
-    return res;
+    return size;
 }
 
-static int xmp_write(const char *path, const char *buf, size_t size,
-                     off_t offset, struct fuse_file_info *fi)
-{
-    int fd;
-    int res;
-
-    (void) fi;
-    fd = open(path, O_WRONLY);
-    if (fd == -1)
-        return -errno;
-
-    res = pwrite(fd, buf, size, offset);
-    if (res == -1)
-        res = -errno;
-
-    close(fd);
-    return res;
-}
-
-static int xmp_statfs(const char *path, struct statvfs *stbuf)
-{
-    int res;
-
-    res = statvfs(path, stbuf);
-    if (res == -1)
-        return -errno;
-
-    return 0;
-}
-
-static int xmp_release(const char *path, struct fuse_file_info *fi)
-{
-    /* Just a stub.  This method is optional and can safely be left
-       unimplemented */
-
-    (void) path;
-    (void) fi;
-    return 0;
-}
-
-static int xmp_fsync(const char *path, int isdatasync,
-                     struct fuse_file_info *fi)
-{
-    /* Just a stub.  This method is optional and can safely be left
-       unimplemented */
-
-    (void) path;
-    (void) isdatasync;
-    (void) fi;
-    return 0;
-}
-
-#ifdef HAVE_SETXATTR
-/* xattr operations are optional and can safely be left unimplemented */
-static int xmp_setxattr(const char *path, const char *name, const char *value,
-                        size_t size, int flags)
-{
-    int res = lsetxattr(path, name, value, size, flags);
-    if (res == -1)
-        return -errno;
-    return 0;
-}
-
-static int xmp_getxattr(const char *path, const char *name, char *value,
-                    size_t size)
-{
-    int res = lgetxattr(path, name, value, size);
-    if (res == -1)
-        return -errno;
-    return res;
-}
-
-static int xmp_listxattr(const char *path, char *list, size_t size)
-{
-    int res = llistxattr(path, list, size);
-    if (res == -1)
-        return -errno;
-    return res;
-}
-
-static int xmp_removexattr(const char *path, const char *name)
-{
-    int res = lremovexattr(path, name);
-    if (res == -1)
-        return -errno;
-    return 0;
-}
-#endif /* HAVE_SETXATTR */
-
-static struct fuse_operations xmp_oper = {
-    .getattr	= xmp_getattr,
-    .access	= xmp_access,
-    .readlink	= xmp_readlink,
-    .readdir	= xmp_readdir,
-    .mknod	= xmp_mknod,
-    .mkdir	= xmp_mkdir,
-    .symlink	= xmp_symlink,
-    .unlink	= xmp_unlink,
-    .rmdir	= xmp_rmdir,
-    .rename	= xmp_rename,
-    .link	= xmp_link,
-    .chmod	= xmp_chmod,
-    .chown	= xmp_chown,
-    .truncate	= xmp_truncate,
-    .utimens	= xmp_utimens,
-    .open	= xmp_open,
-    .read	= xmp_read,
-    .write	= xmp_write,
-    .statfs	= xmp_statfs,
-    .release	= xmp_release,
-    .fsync	= xmp_fsync,
-#ifdef HAVE_SETXATTR
-    .setxattr	= xmp_setxattr,
-    .getxattr	= xmp_getxattr,
-    .listxattr	= xmp_listxattr,
-    .removexattr= xmp_removexattr,
-#endif
+static struct fuse_operations hello_oper = {
+    .getattr	= hello_getattr,
+    .readdir	= hello_readdir,
+    .open	= hello_open,
+    .read	= hello_read,
 };
 
 int main(int argc, char *argv[])
 {
-	int res;
-	struct stat buffer;
-	//DIR *dp;
-	struct fuse_file_info fi;
+  printf("main\n");
+
+TREE* tree;
+	int length;
+
+	tree = create_tree();
+
+	static const char filename[] = "path.txt";
+	FILE *file = fopen (filename, "r");
+	if(file != NULL) {
+		char line [128]; /* or other suitable maximum line size */
+		while (fgets(line, sizeof line, file) != NULL){ /* read a line */
+			//printf("%s",line); /* write the line */
+			length = strlen(line);
+			if(line[length-1] == '\n')
+   				line[length-1] = 0;
+			add_node(tree, line);
+		}
+		fclose (file);
+	}else{
+		perror (filename); /* why didn't the file open? */
+	}
+
+	   print_tree(tree);
 
 
-	res = xmp_mkdir("/testing", 493); 
-	res = xmp_getattr("/testing", &buffer); //cd
-	//res = readdir(dp);
-	res = xmp_mkdir("/testing/test", 493);
-	//res = xmp_open("testing/test/mytest.txt",&fi); 
-
-	//res = xmp_readdir("/testing", dp, de, offset, fi);
-
-//xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
-	
-    umask(0);
-    return fuse_main(argc, argv, &xmp_oper, NULL);
+    return fuse_main(argc, argv, &hello_oper, NULL);
 }
